21c606a5173c23820f0bf7b4d6a3759faee265aa
Gordian Schuster <go.schuster@gmail.com>
2020-11-17 21:49:59 +0000

    changes on adminpage view of users

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gordianschuster.de',
+            password='password1234'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gordianschuster.com',
+            password='test1234',
+            name='testuser Usertester'
+        )
+
+    def test_users_listed(self):
+        """test that users are listed in admin page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # Above : creates url like /admin/core/user/2 if ID=2
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
